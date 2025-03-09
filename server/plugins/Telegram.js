321973299a96a15bef5c1567962929333a1a9036
Element <97992918+elementbyte@users.noreply.github.com>
2025-03-09 20:59:20 +1100

    Attempt to drop duplicates by using a set

--- server/plugins/Telegram.js
+++ server/plugins/Telegram.js
@@ -2,33 +2,43 @@ var telegram = require('telegram-bot-api');
 var util = require('util');
 var logger = require('../log');
 
+const sentMessages = new Set();
+
 function run(trigger, scope, data, config, callback) {
     var tConf = data.pluginconf.Telegram;
     if (tConf && tConf.enable) {
         var telekey = config.teleAPIKEY;
         var t = new telegram({
-          token: telekey
+            token: telekey
         });
+
         if (tConf.chat == 0 || !tConf.chat) {
             logger.main.error('Telegram: ' + data.address + ' No ChatID key set. Please enter ChatID.');
-            callback();
-        } else {
-            //Notification formatted in Markdown for pretty notifications
-            var notificationText = `*${data.agency} - ${data.alias}*\n` + 
-                                    `Message: ${data.message}`;
-            
-            t.sendMessage({
-                chat_id: tConf.chat,
-                text: notificationText,
-                parse_mode: "Markdown"
-            }).then(function(data) {
-                logger.main.debug('Telegram: ' + util.inspect(data, false, null));
-                callback();
-            }).catch(function(err) {
-                logger.main.error('Telegram: ' + err);
-                callback();
-            });
+            return callback();
         }
+
+        var notificationText = `*${data.agency} - ${data.alias}*\n` + 
+                                `Message: ${data.message}`;
+
+        if (sentMessages.has(data.message)) {
+            logger.main.debug('Telegram: Dropping duplicate message: ' + data.message);
+            return callback();
+        }
+
+        sentMessages.add(data.message);
+        setTimeout(() => sentMessages.delete(data.message), 2 * 60 * 1000); 
+
+        t.sendMessage({
+            chat_id: tConf.chat,
+            text: notificationText,
+            parse_mode: "Markdown"
+        }).then(function(response) {
+            logger.main.debug('Telegram: ' + util.inspect(response, false, null));
+            callback();
+        }).catch(function(err) {
+            logger.main.error('Telegram: ' + err);
+            callback();
+        });
     } else {
         callback();
     }
@@ -36,4 +46,4 @@ function run(trigger, scope, data, config, callback) {
 
 module.exports = {
     run: run
-}
+};
