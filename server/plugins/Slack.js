0deb5fcefa062ee7b118c0b6c6f560485ed3b8b1
Element <97992918+elementbyte@users.noreply.github.com>
2025-04-07 14:05:44 +1000

    Update Slack.js
    Drop some of this giberish decode

--- server/plugins/Slack.js
+++ server/plugins/Slack.js
@@ -9,9 +9,12 @@ let healthCheckConfig = null;
 
 const HEALTH_CHECK_CHANNEL = '#all-pagers';
 const DUTY_OFFICERS_CHANNEL = '#seac-duty-officers';
-const DO_KEYWORDS = ['LGDO', 'MODO', 'STDO'];
+const BAD_DECODE_CHANNEL = '#possible-bad-decodes';
 
-// Sends health check summary to the all-pagers channel
+const DO_KEYWORDS = ['LGDO', 'MODO', 'STDO'];
+const BAD_DECODE_ALPHA_THRESHOLD = 8;
+
+// Send a health check summary every 12 hours
 function sendHealthCheck() {
     if (!healthCheckConfig) {
         logger.main.error('Health Check: Config not available.');
@@ -39,70 +42,80 @@ function sendHealthCheck() {
 function run(trigger, scope, data, config, callback) {
     const slConf = data.pluginconf.Slack;
 
-    if (slConf && slConf.enable) {
-        if (!config.bottoken || !slConf.channel) {
-            logger.main.error('Slack: ' + data.address + ' No Bot Token or Channel Set.');
-            return callback();
-        }
+    if (!slConf || !slConf.enable) return callback();
+    if (!config.bottoken || !slConf.channel) {
+        logger.main.error('Slack: ' + data.address + ' No Bot Token or Channel Set.');
+        return callback();
+    }
 
-        if (!data.message || data.message.length < 7) {
-            logger.main.error('Slack: ' + data.address + ' message is too short to process.');
-            return callback();
-        }
+    if (!data.message || data.message.length < 7) {
+        logger.main.error('Slack: ' + data.address + ' message is too short to process.');
+        return callback();
+    }
 
-        if (!healthCheckTimerStarted) {
-            healthCheckConfig = config;
-            setInterval(sendHealthCheck, 12 * 60 * 60 * 1000);
-            healthCheckTimerStarted = true;
-        }
+    if (!healthCheckTimerStarted) {
+        healthCheckConfig = config;
+        setInterval(sendHealthCheck, 12 * 60 * 60 * 1000);
+        healthCheckTimerStarted = true;
+    }
 
-        if (data.message.toLowerCase().includes("test")) {
-            testMessages.push(data.message);
-            logger.main.debug('Slack: Test message detected, added to health check list.');
-            return callback();
-        }
+    const slackClient = new Slack({ token: config.bottoken });
+    const upperMessage = data.message.toUpperCase();
 
-        const slackClient = new Slack({ token: config.bottoken });
+    // If test message, log it and skip sending
+    if (upperMessage.includes("TEST")) {
+        testMessages.push(data.message);
+        logger.main.debug('Slack: Test message detected, added to health check list.');
+        return callback();
+    }
 
-        const messageKey = data.message.slice(3);
-        if (sentMessages.has(messageKey)) {
-            logger.main.debug('Slack: Dropping duplicate message: ' + data.message);
-            return callback();
-        }
+    // Deduplication check (ignoring first 3 chars)
+    const messageKey = data.message.slice(3);
+    if (sentMessages.has(messageKey)) {
+        logger.main.debug('Slack: Dropping duplicate message: ' + data.message);
+        return callback();
+    }
+    sentMessages.add(messageKey);
+    setTimeout(() => sentMessages.delete(messageKey), 5 * 60 * 1000); // Expire after 5 minutes
 
-        sentMessages.add(messageKey);
-        setTimeout(() => sentMessages.delete(messageKey), 5 * 60 * 1000);
+    // Format the message
+    const formattedMessage = `*${data.agency}*\n\`\`\`\n${data.message}\n\`\`\``;
 
-        const formattedMessage = `*${data.agency} - ${data.alias}*\n\`\`\`\n${data.message}\n\`\`\``;
+    // Count alphabetic characters
+    const alphaCount = (data.message.match(/[a-zA-Z]/g) || []).length;
 
-        // Function to send message to any channel
-        const sendToChannel = (channel) => {
-            return slackClient.chat.postMessage({
-                channel: channel,
-                text: formattedMessage
-            });
-        };
+    // Message sending logic
+    const sendToChannel = (channel) => {
+        return slackClient.chat.postMessage({
+            channel: channel,
+            text: formattedMessage
+        });
+    };
 
-        // Always send to the configured channel
-        sendToChannel(slConf.channel)
-            .then((response) => {
-                logger.main.debug('Slack: ' + util.inspect(response, false, null));
-
-                // If the message contains any DO keywords, also send to the duty officers channel
-                const upperMessage = data.message.toUpperCase();
-                if (DO_KEYWORDS.some(keyword => upperMessage.includes(keyword))) {
-                    return sendToChannel(DUTY_OFFICERS_CHANNEL);
-                }
-            })
+    // If it's likely a bad decode, send only to BAD_DECODE_CHANNEL
+    if (alphaCount < BAD_DECODE_ALPHA_THRESHOLD) {
+        logger.main.debug(`Slack: Message flagged as possible bad decode (${alphaCount} letters), sending to ${BAD_DECODE_CHANNEL}`);
+        return sendToChannel(BAD_DECODE_CHANNEL)
             .then(() => callback())
             .catch((err) => {
-                logger.main.error('Slack: ' + err);
+                logger.main.error('Slack (Bad Decode): ' + err);
                 callback();
             });
-
-    } else {
-        callback();
     }
+
+    // Send to normal configured channel
+    sendToChannel(slConf.channel)
+        .then(() => {
+            // If duty officer keywords present, also send to DO channel
+            if (DO_KEYWORDS.some(keyword => upperMessage.includes(keyword))) {
+                return sendToChannel(DUTY_OFFICERS_CHANNEL);
+            }
+        })
+        .then(() => callback())
+        .catch((err) => {
+            logger.main.error('Slack: ' + err);
+            callback();
+        });
 }
 
 module.exports = {
